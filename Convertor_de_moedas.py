351eac9ba49148c259ef339a7b1b979d4b4aca2b
Jorge Gabriel <127042883+J0rgeGabriel@users.noreply.github.com>
2023-04-30 01:50:46 -0300

    inserindo perguntas de continuação das conversoes

--- Convertor_de_moedas.py
+++ Convertor_de_moedas.py
@@ -1,9 +1,8 @@
 while True:
     real = float(input("Digite quantos reais você gostaria de converter: R$").replace(',','.'))
     
-    while real == 0:
-        while True:
-            real = float(input("Digite quantos reais você gostaria de converter: R$").replace(',', '.'))
+    while real <= 0:
+            real = float(input("Não é possivel converter zero!! Digite quantos reais você gostaria de converter: R$").replace(',', '.'))
 
     opcao = input("Para qual moeda você gostaria de converter? Escolha uma opção a seguir: \n"
                 "1 - Dólar Americano\n"
@@ -18,14 +17,15 @@ while True:
 
     while opcao not in ["0", "1", "2", "3", "4", "5", "6"]:
         opcao = input("Para qual moeda você gostaria de converter? Escolha uma opção a seguir: \n"
-            "1 - Dólar Americano\n"
-            "2 - Dolar Australiano\n"
-            "3 - Dólar Canadense\n"
-            "4 - Euro\n"
-            "5 - Libra\n"
-            "6 - Iene\n"
-            "0 - Parar de converter\n"
-            "Para qual moeda gostaria de converter? ")
+                    "1 - Dólar Americano\n"
+                    "2 - Dolar Australiano\n"
+                    "3 - Dólar Canadense\n"
+                    "4 - Euro\n"
+                    "5 - Libra\n"
+                    "6 - Iene\n"
+                    "0 - Parar de converter\n"
+                    "Para qual moeda gostaria de converter? ")
+        print()
 
     if opcao == "1":
         dolar = round(real * 0.20, 2)
@@ -53,4 +53,11 @@ while True:
 
     elif opcao == "0":
         print("Conversões encerradas")
+        
+        
+    continuar = input("Você deseja continuar realizando conversões? [S/N]").upper()
+    if continuar == "S":
+        continue
+    else:
+        print("Encerrando as conversões")
         break
